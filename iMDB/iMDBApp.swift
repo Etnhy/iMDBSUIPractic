87481fbbc0c5cf0889885aee87d90b908d266063
Evhenii Mahlena <magellan7777@icloud.com>
2022-06-08 09:37:05 +0300

    Initial Commit

---
+++ iMDB/iMDBApp.swift
@@ -0,0 +1,17 @@
+//
+//  iMDBApp.swift
+//  iMDB
+//
+//  Created by Evhenii Mahlena on 08.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct iMDBApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
